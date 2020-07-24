f35c92888580d22dc5b7cdb3b6dd457e0c2a18a1
RajithaGayashan <rajithagayashan95@gmail.com>
2020-07-24 16:53:01 +0530

    form created

---
+++ angular-firebase-crud/src/app/shared/customer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { FormControl , FormGroup } from '@angular/Forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor() { }
+  form=new FormGroup({
+    $key:new FormControl(null),
+    fullName:new FormControl(''),
+    email:new FormControl(''),
+    mobile:new FormControl(''),
+    location:new FormControl('')
+
+  })
+}
