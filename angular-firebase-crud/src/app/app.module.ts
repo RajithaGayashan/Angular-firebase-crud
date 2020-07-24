362977f53847ea739d2804b1e10d274d45714849
RajithaGayashan <rajithagayashan95@gmail.com>
2020-07-24 20:20:30 +0530

    form updated

--- angular-firebase-crud/src/app/app.module.ts
+++ angular-firebase-crud/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { CustomerService } from './shared/customer.service'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [CustomerService],
   bootstrap: [AppComponent]
